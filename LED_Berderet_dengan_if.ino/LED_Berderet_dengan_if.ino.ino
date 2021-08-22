6d3bc38cf1b778064a22dc742eced51aa5e963f6
Ery Azra <87509292+eryazra@users.noreply.github.com>
2021-08-22 17:46:43 +0700

    Add files via upload

---
+++ LED_Berderet_dengan_if.ino/LED_Berderet_dengan_if.ino.ino
@@ -0,0 +1,29 @@
+//inisialisasi jumlah LED
+const int numLED=4;
+//LED 1,2,3,4 jadi 1 variabel
+//dengan alamat index 0,1,2,3
+const int pinLED[numLED]={8,9,10,11};
+
+void setup() {
+  // put your setup code here, to run once:
+//inisialisasi semua pin LED sebagai OUTPUT
+for(int i=0; i<4; i++){
+  pinMode(pinLED[i],OUTPUT);
+}
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+//hidupkan LED indeks 0 hingga 2 satu-persatu
+for(int i=0; i<3; i++){
+digitalWrite(pinLED[i],HIGH);
+delay(200);
+digitalWrite(pinLED[i],LOW);
+}
+//hidupkan LED indeks 3 hingga 1 satu-persatu
+for (int i=3; i>0; i--){
+digitalWrite(pinLED[i],HIGH);
+delay(200);
+digitalWrite(pinLED[i],LOW);
+}
+}
---
+++ LED_berkedip_dengan_if/LED_berkedip_dengan_if.ino
@@ -0,0 +1,23 @@
+//pin 8 untuk LED
+const int pinLED=8;
+
+void setup() {
+  // put your setup code here, to run once:
+pinMode(pinLED,OUTPUT);
+}
+//awal time delay 1000| 1 detik
+int timeDelay=1000;
+
+void loop() {
+//setiap looping nilai timeDelay di kurang 100
+timeDelay=timeDelay-100;
+
+// jika bernilai 0/negatif maka akan direset ke awal(1000)
+if(timeDelay<=0){
+  timeDelay=1000;
+}
+digitalWrite(pinLED, HIGH);
+delay(timeDelay);
+digitalWrite(pinLED, LOW);
+delay(timeDelay);
+}
